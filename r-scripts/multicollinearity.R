3df9f12260d5506c8bb0ea3d8d144c65e0f4534c
Vilhelm Stiernstedt <Mille@vilhelms-mbp.home>
2018-04-03 19:03:11 +0200

    cleaning

--- r-scripts/multicollinearity.R
+++
@@ -1,6 +0,0 @@
-# Assess Multicollinearity
-linear_combinations <- findLinearCombos(data.matrix(dt_test))
-colnames(dt_test)[linear_combinations$linearCombos[[1]]]
-colnames(dt_test)[linear_combinations$linearCombos[[2]]]
-colnames(dt_test)[linear_combinations$remove]
-# correlation due to house size. -> no need to explain what predict house price -> thus no need to drop columns
